be35071fd4da2f8b1215bbb2a454cc38e7920284
kwonwoohee <kwon.wh02@gmail.com>
2021-10-12 12:35:30 +0900

    Chap05(Google Login(1))

--- src/main/java/com/book/review/web/IndexController.java
+++ src/main/java/com/book/review/web/IndexController.java
@@ -1,27 +1,42 @@
 package com.book.review.web;
 
+import com.book.review.config.auth.dto.SessionUser;
 import com.book.review.service.PostsService;
 import com.book.review.web.dto.PostsResponseDto;
 import com.book.review.web.dto.PostsUpdateRequestDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+    private SessionUser user;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("myUserName", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String save() {
+    public String save(Model model) {
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "posts-save";
     }
 
